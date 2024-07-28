2b0be0a6a58156ae89855293e4bf2b3266e9c350
CODEdragALon <102843684+CODEdragALon@users.noreply.github.com>
2024-07-28 09:53:40 -0400

    Update pins.js

--- pins.js
+++ pins.js
@@ -4,7 +4,7 @@ const provinces = {
     "bc": "British Columbia",
     "on": "Ontario",
     "nt": "Northwest Territories",
-    "qc": "Qubec",
+    "qc": "Quebec",
     "mb": "Manitoba",
     "sk": "Saskatchewan",
     "ab": "Alberta",
@@ -20,13 +20,11 @@ function loadData() {
     for (let i=0; i<mydata.length; i++){
         let wildFire = mydata[i]
 
-        L.circleMarker([wildFire.lat, wildFire.lon], {
+        pin = L.circleMarker([wildFire.lat, wildFire.lon], {
             radius: 5
         }).addTo(map)
         .bindPopup(`${wildFire.firename}, in ${provinces[wildFire.agency]}`) 
         .openPopup();
-        // L.marker([wildFire.lat, wildFire[i].lon]).addTo(map)
-        // .bindPopup(`${wildFire.firename}, in ${wildfire.agency}`) 
-        // .openPopup();
+        pin.setStyle({color: 'red'});
     }
 }
