1e0a852996bcc715c0206b0e6add66101eb9770d
Ciftelia <120532343+Ciftelia@users.noreply.github.com>
2024-07-28 13:43:20 -0400

    working-temp

--- userlocator.js
+++ userlocator.js
@@ -90,8 +90,6 @@ navigator.geolocation.getCurrentPosition(function(location) {
      map.addControl(searchControl);
      map.addControl(new L.Control.Fullscreen());
 
-
-
 }, {
     timeout: 10000,
     maximumAge: 1000,
