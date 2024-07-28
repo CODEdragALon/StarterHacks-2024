cb7fd29221254937edb7a6bb03ee3857b24cdfc9
Ciftelia <120532343+Ciftelia@users.noreply.github.com>
2024-07-27 21:31:13 -0400

    copilot changes

--- firepins.js
+++ firepins.js
@@ -1,34 +1,30 @@
-const fs = require("fs");
 const path = "activefires.csv";
 
-// Get coordinates of firesS
-fs.readFile(path, "utf8", (err, data) => {
-    if(err) {
+fetch(path)
+    .then(response => response.text())
+    .then(data => {
+        const lines = data.split("\n");
+        const coordinates = [];
+
+        lines.forEach((line) => {
+            const fields = line.split(",");
+
+            const lat = parseFloat(fields[2]);
+            const lon = parseFloat(fields[3]);
+            const latAndLon = [lat, lon];
+
+            coordinates.push(latAndLon);
+        });
+
+        coordinates.shift(); // Remove header if present
+
+        // Add coordinates of fires to map
+        coordinates.forEach((latAndLon) => {
+            var marker = L.marker(latAndLon).addTo(map);
+            marker.bindPopup('A pretty CSS popup.<br> Easily customizable.');
+            marker.openPopup();
+        });
+    })
+    .catch(err => {
         console.error("Error while reading:", err);
-        return;
-    }
-
-    const lines = data.split("\n");
-    const coordinates = [];
-
-    lines.forEach((line) => {
-        const feilds = line.split(",");
-
-        const lat = parseFloat(feilds[2]);
-        const lon = parseFloat(feilds[3]);
-        const latAndLon = [lat, lon];
-
-        coordinates.push(latAndLon);
     });
-
-    coordinates.shift();
-    //console.log(coordinates);
-
-});
-
-// Add coordinates of fires to map
-for (var i=0; i < coordinates.length; i++) {
-    L.marker([coordinates[i][0], coordinates[i][1]]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
-}
